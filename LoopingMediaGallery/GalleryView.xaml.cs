bc640b71f24f4993e9aa6185b23932a3c0470651
LanceErickson <lance.erickson@gmail.com>
2015-08-29 19:11:37 -0600

    Rest of the recovered project.

---
+++ LoopingMediaGallery/GalleryView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoopingMediaGallery
+{
+    /// <summary>
+    /// Interaction logic for GalleryView.xaml
+    /// </summary>
+    public partial class GalleryView : Window
+    {
+        MainWindowViewModel viewModel;
+
+        public GalleryView()
+        {
+            InitializeComponent();
+        }
+
+        public GalleryView(MainWindowViewModel viewModel) : this()
+        {
+            this.viewModel = viewModel;
+        }
+
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowStyle = WindowStyle.None;
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            loopingMediaControl.Dispose();
+        }
+    }
+}
---
+++ LoopingMediaGallery/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace LoopingMediaGallery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+			var galleryView = new GalleryView();
+			galleryView.Show();
+
+			((MainWindowViewModel)DataContext).AddGallery(galleryView);
+
+			((MainWindowViewModel)DataContext).PreviewPane = this.loopPreviewPane;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            ((MainWindowViewModel)DataContext).Dispose();
+        }
+
+        private void btnStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)((Button)sender).Content == "Start")
+                ((Button)sender).Content = "Stop";
+            else
+                ((Button)sender).Content = "Start";
+        }
+
+		private void btnOpen_Click(object sender, RoutedEventArgs e)
+		{
+			var dlg = new CommonOpenFileDialog();
+			dlg.Title = "Select a path to monitor";
+			dlg.IsFolderPicker = true;
+
+			dlg.AddToMostRecentlyUsedList = false;
+			dlg.AllowNonFileSystemItems = false;
+			dlg.EnsureFileExists = true;
+			dlg.EnsurePathExists = true;
+			dlg.EnsureReadOnly = false;
+			dlg.EnsureValidNames = true;
+			dlg.Multiselect = false;
+			dlg.ShowPlacesList = true;
+
+			if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+			{
+				var folder = dlg.FileName;
+
+				((MainWindowViewModel)DataContext).FolderPath = folder;
+            }
+		}
+	}
+}
